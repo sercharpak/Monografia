e0ed78a03f30bcd9b3e4ec9b0f727f591583ec14
Sergio <sergiocharpak@gmail.com>
2016-05-06 19:33:00 -0500

    Added the 3d plots of some regions, and the overall ones

---
+++ Results/06_05_2016/pipeline_plot_groups.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+
+#Plotting the detected groups
+#By Sergio Daniel Hernandez Charpak
+
+#-------------------------------------------
+#Imports
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import sys
+#-------------------------------------------
+# USAGE = "python pipeline_histograms.py file_valid_groups output_folder OPT "
+#File example: group_valid_positions_FA_Trace_seeds_FA_0.6_Trace_1.0_search_FA_0.8_Trace_0.0.dat
+USAGE = "python pipepline_plt_groups.py file_valid_groups output_folder OPT(1 for plotting all 0 for not) "
+
+#-------------------------------------------
+#Functions
+#Function to extract the thresholds from a group filename
+def get_thresholds(filename):
+    file_Dat = filename.strip(".dat")
+    file_array = file_Dat.split("_")
+    thresh_search_FA = float(file_array[12])
+    thresh_search_Trace = float(file_array[14])
+    thresh_seeds_FA = float(file_array[7])
+    thresh_seeds_Trace = float(file_array[9])
+    return thresh_search_FA, thresh_search_Trace, thresh_seeds_FA, thresh_seeds_Trace
+#-------------------------------------------
+if(len(sys.argv)!=4):
+    print "Please use correctly"
+    print USAGE
+    sys.exit()
+
+inputfile = sys.argv[1]
+output_folder = sys.argv[2]
+print "Processing the file: " + inputfile
+thresh_search_FA, thresh_search_Trace, thresh_seeds_FA, thresh_seeds_Trace = get_thresholds(inputfile)
+group_data = np.loadtxt(inputfile)
+n_particles = len(group_data)
+print "There are", n_particles,"particles for the file", inputfile
+
+#------------------------------------
+#First we plot all the particles
+from mpl_toolkits.mplot3d import Axes3D
+#Particles coordinates
+x_particles_all = group_data[:,0]
+y_particles_all = group_data[:,1]
+z_particles_all = group_data[:,2]
+group_particle = group_data[:,5]
+n_groups = len(group_particle)
+#TODO This should be improved
+n_x, n_y, n_z = 256,256,256
+
+fig = plt.figure(figsize=(25, 25))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x_particles_all,y_particles_all,z_particles_all,s=2, c=group_particle)
+ax.set_xlim3d(0, n_x)
+ax.set_ylim3d(0, n_y)
+ax.set_zlim3d(0, n_z)
+ax.set_xlabel('i', size=30)
+ax.set_ylabel('j', size=30)
+ax.set_zlabel('k', size=30)
+ax.set_title('Regions obtained at scale with the simulation for parameters \n Seeds (FA:'+str(thresh_seeds_FA)+
+' $\delta$:'+str(thresh_seeds_Trace)+') \n Search (FA:'+str(thresh_search_FA)+
+' $\delta$:'+str(thresh_search_Trace)+')', size=35)
+ax.tick_params(axis='both', which='major', labelsize=30)
+plt.savefig(output_folder+"regions_all_seeds_FA_" + str(thresh_seeds_FA) + "_Trace_" + str(thresh_seeds_Trace) + "_search_FA_" + str(thresh_search_FA) + "_Trace_" + str(thresh_search_Trace) + ".png",format = 'png')
+plt.close(fig)
+
+#-------------------------------
+#If the option 1 was selected we plot each of the regions
+#Now let us plot each region.
+print "The option selected is", sys.argv[3]
+if(int(sys.argv[3]) == 1):
+    id_groups = list(set(group_particle))
+    id_groups = np.array(id_groups)
+    for id_group in id_groups:
+        print "Is processing group number", id_group
+        index = where(group_particle==id_group)
+        index = index[0]
+        x_particles = x_particles_all[index]
+        y_particles = y_particles_all[index]
+        z_particles = z_particles_all[index]
+        x_max = np.max(x_particles)
+        x_min = np.min(x_particles)
+        y_max = np.max(y_particles)
+        y_min = np.min(y_particles)
+        z_max = np.max(z_particles)
+        z_min = np.min(z_particles)
+        #Only the region in its region
+        fig = plt.figure(figsize=(25, 25))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x_particles,y_particles,z_particles,s=20)
+        ax.set_xlim3d(x_min, x_max)
+        ax.set_ylim3d(y_min, y_max)
+        ax.set_zlim3d(z_min, z_max)
+        ax.set_xlabel('i', size=30)
+        ax.set_ylabel('j', size=30)
+        ax.set_zlabel('k', size=30)
+        ax.set_title('Region '+str(id_group)+' for parameters \n Seeds (FA:'+str(thresh_seeds_FA)+
+        ' $\delta$:'+str(thresh_seeds_Trace)+') \n Search (FA:'+str(thresh_search_FA)+
+        ' $\delta$:'+str(thresh_search_Trace)+')', size=35)
+        ax.tick_params(axis='both', which='major', labelsize=30)
+        plt.savefig(output_folder+"region_"+str(id_group)+"_small_seeds_FA_" + str(thresh_seeds_FA) + "_Trace_" + str(thresh_seeds_Trace) + "_search_FA_" + str(thresh_search_FA) + "_Trace_" + str(thresh_search_Trace) + ".png",format = 'png')
+        plt.close(fig)
+        #At scale with the simulation
+        fig = plt.figure(figsize=(25, 25))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x_particles,y_particles,z_particles,s=2)
+        ax.set_xlim3d(0, n_x)
+        ax.set_ylim3d(0, n_y)
+        ax.set_zlim3d(0, n_z)
+        ax.set_xlabel('i', size=30)
+        ax.set_ylabel('j', size=30)
+        ax.set_zlabel('k', size=30)
+        ax.set_title('Region '+str(id_group)+' at scale in the simulation for parameters \n Seeds (FA:'+str(thresh_seeds_FA)+
+        ' $\delta$:'+str(thresh_seeds_Trace)+') \n Search (FA:'+str(thresh_search_FA)+
+        ' $\delta$:'+str(thresh_search_Trace)+')', size=35)
+        ax.tick_params(axis='both', which='major', labelsize=30)
+        plt.savefig(output_folder+"region_"+str(id_group)+"_scale_seeds_FA_" + str(thresh_seeds_FA) + "_Trace_" + str(thresh_seeds_Trace) + "_search_FA_" + str(thresh_search_FA) + "_Trace_" + str(thresh_search_Trace) + ".png",format = 'png')
+        plt.close(fig)
+        
+#The group 0.0 seems like noise. We will proceed to make the 3d plot of the regions excluding this one.
+index = where(group_particle==0.0)
+index = index[0]
+x_particles_all = np.delete(x_particles_all, index, axis=0)
+y_particles_all = np.delete(y_particles_all, index, axis=0)
+z_particles_all = np.delete(z_particles_all, index, axis=0)
+group_particle = np.delete(group_particle, index, axis=0)
+#The plot
+fig = plt.figure(figsize=(25, 25))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x_particles_all,y_particles_all,z_particles_all,s=2, c=group_particle)
+ax.set_xlim3d(0, n_x)
+ax.set_ylim3d(0, n_y)
+ax.set_zlim3d(0, n_z)
+ax.set_xlabel('i', size=30)
+ax.set_ylabel('j', size=30)
+ax.set_zlabel('k', size=30)
+ax.set_title('Regions obtained at scale with the simulation for parameters \n Seeds (FA:'+str(thresh_seeds_FA)+
+' $\delta$:'+str(thresh_seeds_Trace)+') \n Search (FA:'+str(thresh_search_FA)+
+' $\delta$:'+str(thresh_search_Trace)+')', size=35)
+ax.tick_params(axis='both', which='major', labelsize=30)
+plt.savefig(output_folder+"regions_nonoise_seeds_FA_" + str(thresh_seeds_FA) + "_Trace_" + str(thresh_seeds_Trace) + "_search_FA_" + str(thresh_search_FA) + "_Trace_" + str(thresh_search_Trace) + ".png",format = 'png')
+plt.close(fig)
---
+++ Results/06_05_2016/pipeline_script_1.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+#
+#Written by Sergio Hernandez Charpak
+#
+
+# Usage
+# python pipeline_script_1.py file_eigen_vale_1 file_eigen_vale_2 file_eigen_vale_3 FA_thresh_value Div_thresh_value
+#
+
+USAGE = "python pipeline_script_1.py file_eigen_vale_1 file_eigen_vale_2 file_eigen_vale_3 FA_thresh_value Div_thresh_value"
+
+#Imports
+#General Imports
+#Reading the CIC
+from ast import literal_eval
+from struct import *
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+#Arguments
+import sys
+
+#Function to read the EigenValues from the v_web CIC outpu
+def read_CIC_scalar(filename):
+    f = open(filename, "rb")
+    dumb = f.read(38)
+
+    dumb = f.read(4)
+    n_x = f.read(4)
+    n_y = f.read(4)
+    n_z = f.read(4)
+    nodes = f.read(8)
+    x0 = f.read(4)
+    y0 = f.read(4)
+    z0 = f.read(4)
+    dx = f.read(4)
+    dy = f.read(4)
+    dz = f.read(4)
+    dumb = f.read(4)
+
+    n_x = (unpack('i', n_x))[0]
+    n_y = (unpack('i', n_y))[0]
+    n_z = (unpack('i', n_z))[0]
+    nodes = (unpack('q', nodes))[0]
+    dx = (unpack('f', dx))[0]
+    dy = (unpack('f', dy))[0]
+    dz = (unpack('f', dz))[0]
+    x0 = (unpack('f', x0))[0]
+    y0 = (unpack('f', y0))[0]
+    z0 = (unpack('f', z0))[0]
+    print n_x, n_y, n_z, nodes, dx, dy, dz
+
+    total_nodes = n_x * n_y *n_z
+    dumb = f.read(4)
+    array_data = f.read(total_nodes*4)
+    dumb = f.read(4)
+    format_s = str(total_nodes)+'f'
+    array_data = unpack(format_s, array_data)
+    f.close()
+    array_data  = np.array(array_data)
+    array_data.resize(n_z,n_y,n_x)
+    array_data = array_data.transpose()
+    return array_data
+
+#---------------------------------------------------------
+
+if(len(sys.argv)!=6):
+    print "Please use correctly"
+    print USAGE
+    sys.exit()
+
+inputfile_1 = sys.argv[1]
+inputfile_2 = sys.argv[2]
+inputfile_3 = sys.argv[3]
+
+print "Preparing to read the eigenvalues"
+print inputfile_1,inputfile_2,inputfile_3
+
+grid_1 = read_CIC_scalar(inputfile_1)
+grid_2 = read_CIC_scalar(inputfile_2)
+grid_3 = read_CIC_scalar(inputfile_3)
+
+print "Eigenvalues read"
+
+print "Forming the FA and the Trace grids"
+
+FA = (grid_1-grid_3)**2  + (grid_2-grid_3)**2  + (grid_1-grid_2)**2
+FA = FA/(grid_1**2 + grid_2**2 + grid_3**2)
+FA = np.sqrt(FA)/np.sqrt(3.0)
+
+Trace = grid_1 + grid_2 + grid_3
+
+print "Fa and Trace matrix formed"
+
+thresh_FA = float(sys.argv[4])
+thresh_Trace = float(sys.argv[5])
+
+print "The Thresholds are: FA, Trace", str(thresh_FA), str(thresh_Trace)
+
+n_x,n_y,n_z = shape(FA)
+
+print "Forming the resulting grid"
+
+cell_list = []
+cells_total = 0
+for i in range (n_x):
+    for j in range (n_y):
+        for k in range (n_z):
+            if((FA[i,j,k]<thresh_FA ) & (Trace[i,j,k]>thresh_Trace)):
+                cells_total +=1
+                cell_list.append([i,j,k, FA[i,j,k], Trace[i,j,k]])
+
+print "Grid formed with number of cells:", cells_total
+
+output_name = '../FoF/src/grid_FA_'+str(thresh_FA)+'_Trace_'+str(thresh_Trace)+'.dat'
+
+print "Writing in file " + output_name
+
+fileout = open(output_name, 'w')
+
+fileout.write("%d\n"%cells_total) #points in total
+fileout.write("%d\n"%cells_total) #points in 'DM'
+fileout.write("0\n") #gas
+fileout.write("0\n") #stars
+fileout.write("0.01\n") # time
+fileout.write("0\n") # nactive
+
+for l in range(cells_total):
+    fileout.write("%f %f %f\n"%(cell_list[l][0],cell_list[l][1],cell_list[l][2]))
+
+fileout.close()
+
+print "Finished writting the file"
+
+#It prints the values of the FA and Trace for the later analysis
+
+output_name = '../FoF/src/values_FA_'+str(thresh_FA)+'_Trace_'+str(thresh_Trace)+'.dat'
+
+print "Writing in FA and Trace values in file " + output_name
+
+fileout = open(output_name, 'w')
+
+fileout.write("%d\n"%cells_total) #points in total
+fileout.write("%d\n"%cells_total) #points in 'DM'
+fileout.write("0\n") #gas
+fileout.write("0\n") #stars
+fileout.write("0.01\n") # time
+fileout.write("0\n") # nactive
+
+l = 0
+for l in range(cells_total):
+    fileout.write("%f %f\n"%(cell_list[l][3],cell_list[l][4]))
+
+fileout.close()
+
+print "Finished writting the file"
